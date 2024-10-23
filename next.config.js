aec8b6eda745db85d60e4c439c82cf7a932519d8
Juliet Chirimumimba <chirimumimbajuliet@gmail.com>
2024-10-23 17:07:39 +0200

    env

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = nextConfig;
 
 module.exports = {
     env:{
-        NEXT_PUBLIC_SUPABASE_URL : 'https://nibasnibyueuopoczhzj.supabase.co',
-        NEXT_PUBLIC_SUPABASE_ANON_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pYmFzbmlieXVldW9wb2N6aHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0MjgxNTUsImV4cCI6MjA0MjAwNDE1NX0.7JqUibwPpg9hdlYNENBHtlDNJwPUZebMwxIIzk3VUmQ',
+        NEXT_PUBLIC_SUPABASE_URL : 'https://oghpmdquumycxxkkjqcb.supabase.co',
+        NEXT_PUBLIC_SUPABASE_ANON_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9naHBtZHF1dW15Y3h4a2tqcWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0OTQyMjQsImV4cCI6MjAzNjA3MDIyNH0.bKJHMadnvVhQ7nPdiujVblywzkyN5K7U4X-uyRshoCUss',
     },
 };
